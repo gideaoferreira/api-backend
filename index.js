50df73082b21eb90328d5011de74e7a1079b9f9f
mrjonc <jonathanlaureano33.jl@gmail.com>
2025-09-01 21:43:07 -0300

    Adicionando a tabela produtos

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import sequelize from './src/database/db-connect.js'
-import createUserController from './src/controllers/user/create-user-controller.js'
-import listUsersController from './src/controllers/user/list-users-controller.js'
-import updateUserController from './src/controllers/user/update-user-controller.js'
-import deleteUserController from './src/controllers/user/delete-user-controller.js'
+import express from "express";
+import cors from "cors";
+import sequelize from "./src/database/db-connect.js";
+import createUserController from "./src/controllers/user/create-user-controller.js";
+import listUsersController from "./src/controllers/user/list-users-controller.js";
+import updateUserController from "./src/controllers/user/update-user-controller.js";
+import deleteUserController from "./src/controllers/user/delete-user-controller.js";
+import createProducts from "./src/controllers/products/create-products-controller.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/users', listUsersController)
-app.post('/user', createUserController)
-app.put('/user/:id', updateUserController)
-app.delete('/user/:id', deleteUserController)
+app.get("/users", listUsersController);
+app.post("/user", createUserController);
+app.put("/user/:id", updateUserController);
+app.delete("/user/:id", deleteUserController);
+
+app.post("/products", createProducts);
 
 app.listen(3000, () => {
-    console.log(`App is running`)
-})
+  console.log(`App is running`);
+});
---
+++ src/controllers/products/create-products-controller.js
@@ -0,0 +1,18 @@
+import Products from "../../models/products.js";
+
+async function createProducts(request, response) {
+  const productsData = request.body;
+
+  const products = await Products.create({
+    name: productsData.name,
+    price: productsData.price,
+    brand: productsData.brand,
+    stock: productsData.stock,
+    description: productsData.description,
+    image: productsData.image,
+  });
+
+  return response.status(201).json(products);
+}
+
+export default createProducts;
